0b74c8aa2daf5ab5f7918a38b1ba54c382050dfa
Müslüm Barış Korkmazer <boran_nesil@hotmail.com>
2023-03-06 00:52:21 +0300

    lang update for en

--- i18n/en/ext.php
+++ i18n/en/ext.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-	'YouTubeChannel2RssFeed' => array(
+	'TwitchChannel2RssFeed' => array(
         'install' => array(
-            'bad_freshrss' => '`YouTubeChannel2RssFeed`required at least FreshRSS version `%s`. (You use FreshRSS version `%s`)',
+            'bad_freshrss' => '`TwitchChannel2RssFeed`required at least FreshRSS version `%s`. (You use FreshRSS version `%s`)',
         ),
 	),
 );
